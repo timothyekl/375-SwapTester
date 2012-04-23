f4e12b4a776c41c45adc794189dc55df63669c07
Tim Ekl <lithium3141@gmail.com>
2012-04-23 03:38:53 -0400

    Implement batch loading test for R2
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- AssignmentTests/R2Test.cs
+++ AssignmentTests/R2Test.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace AssignmentTests
@@ -6,7 +8,41 @@ namespace AssignmentTests
 	[TestFixture()]
 	public class R2Test : ATest
 	{
+		[Test()]
+		public void TestBatchLoad ()
+		{
+			// Use the R1 input file multiple times
+			// The temp file wrapper semantics will give different range names
+			using(TempFileWrapper tempFile1 = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in"),
+			      tempFile2 = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in"),
+			      tempFile3 = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in"))
+			{
+				this.Runner.StartApp (new string[] {tempFile1, tempFile2, tempFile3});
+				this.Runner.WriteInputLine ("");
+				
+				List<String> lines = this.Runner.GetOutputLines ();
+				
+				int headers = 0;
+				int ranges = 0;
+				Regex headerRegex = new Regex("^Range");
+				Regex rangeRegex = new Regex("^\\[(-?[0-9]*)-(-?[0-9]*)\\)");
+				
+				foreach (string line in lines)
+				{
+					if (headerRegex.Matches (line).Count > 0) headers ++;
+					if (rangeRegex.Matches (line).Count > 0) ranges ++;
+				}
+				
+				Assert.AreEqual (3, headers, "Unexpected number of header lines printed");
+				Assert.AreEqual (12, ranges, "Unexpected number of range lines printed");
+			}
+		}
 		
+		[Test()]
+		public void TestFileOutput ()
+		{
+			
+		}
 	}
 }
 
