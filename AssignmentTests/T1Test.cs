990fe5f9276f47655caae83569d142c4b16a484c
Tim Ekl <lithium3141@gmail.com>
2012-04-23 13:15:53 -0400

    Hey look, lambdas! Make T1 test stronger
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- AssignmentTests/T1Test.cs
+++ AssignmentTests/T1Test.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
@@ -17,14 +18,24 @@ namespace AssignmentTests
 				this.Runner.WriteInputLine ("");
 				
 				List<string> lines = this.Runner.GetOutputLines ();
-				lines.RemoveAll (IsWhitespace);
+				lines.RemoveAll ((string line) => {
+					return (new Regex("^\\s*$")).IsMatch (line);
+				});
 				
+				// Check total output count
 				Assert.AreEqual (21, lines.Count, "Unexpected number of output lines");
+				
+				// Check ordering of lines
+				List<string> currentFaceLines = lines.FindAll((string line) => (new Regex("^Current [Ff]ace:")).IsMatch (line));
+				IEnumerable<Int32> currentFaceValues = Enumerable.Select(currentFaceLines, (string line) => Int32.Parse ((new Regex("([0-9]+)$")).Match (line).Captures[0].Value));
+				Int32 last = 999999;
+				foreach(Int32 face in currentFaceValues) {
+					Assert.LessOrEqual(face, last, "Entries are not sorted in descending order by current face value");
+					last = face;
+				}
+				
+				// TODO much more testing could be done here
 			}
-		}
-		
-		private bool IsWhitespace(string line) {
-			return (new Regex("^\\s*$")).IsMatch (line);
 		}
 	}
 }
