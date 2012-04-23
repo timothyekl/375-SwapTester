a1e2471414a87d1c6d5baae134f69443263663c1
Tim Ekl <lithium3141@gmail.com>
2012-04-23 11:28:25 -0400

    Refactor out line categorization
    Also start R2 file output test
    
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- AssignmentTests/R2Test.cs
+++ AssignmentTests/R2Test.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace AssignmentTests
 {
+	public enum R2OutputLineType {
+		R2OutputLineTypeWhitespace,
+		R2OutputLineTypeHeader,
+		R2OutputLineTypeRange
+	};
+	
 	[TestFixture()]
 	public class R2Test : ATest
 	{
@@ -21,27 +28,53 @@ namespace AssignmentTests
 				this.Runner.WriteInputLine ("");
 				
 				List<String> lines = this.Runner.GetOutputLines ();
+				Dictionary<R2OutputLineType,List<string>> categorizedLines = this.CategorizeLines (lines);
 				
-				int headers = 0;
-				int ranges = 0;
-				Regex headerRegex = new Regex("^Range");
-				Regex rangeRegex = new Regex("^\\[(-?[0-9]*)-(-?[0-9]*)\\)");
-				
-				foreach (string line in lines)
-				{
-					if (headerRegex.Matches (line).Count > 0) headers ++;
-					if (rangeRegex.Matches (line).Count > 0) ranges ++;
-				}
-				
-				Assert.AreEqual (3, headers, "Unexpected number of header lines printed");
-				Assert.AreEqual (12, ranges, "Unexpected number of range lines printed");
+				Assert.AreEqual (3, categorizedLines[R2OutputLineType.R2OutputLineTypeHeader].Count, "Unexpected number of header lines printed");
+				Assert.AreEqual (12, categorizedLines[R2OutputLineType.R2OutputLineTypeRange].Count, "Unexpected number of range lines printed");
 			}
 		}
 		
 		[Test()]
 		public void TestFileOutput ()
 		{
+			using(TempFileWrapper outFile = new TempFileWrapper(Path.GetTempFileName ()),
+			      inFile = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in"))
+			{
+				this.Runner.StartApp (new string[] {"-output", outFile, inFile});
+				
+				List<string> lines = this.Runner.GetOutputLines ();
+				Dictionary<R2OutputLineType,List<string>> categorizedLines = this.CategorizeLines (lines);
+				
+				Assert.AreEqual (0, categorizedLines[R2OutputLineType.R2OutputLineTypeRange].Count, "Program provided unexpected output");
+				Assert.AreEqual (0, categorizedLines[R2OutputLineType.R2OutputLineTypeHeader].Count, "Program provided unexpected output");
+				
+				using(FileStream outputReadStream = File.Open(outFile, FileMode.Open))
+				{
+					
+				}
+			}
+		}
+		
+		public Dictionary<R2OutputLineType,List<string>> CategorizeLines (List<string> lines)
+		{
+			Regex headerRegex = new Regex("^Range");
+			Regex rangeRegex = new Regex("^\\[(-?[0-9]*)-(-?[0-9]*)\\)");
+			Regex whitespaceRegex = new Regex("^\\s*$");
 			
+			Dictionary<R2OutputLineType,List<string>> result = new Dictionary<R2OutputLineType, List<string>>();
+			foreach (R2OutputLineType type in Enum.GetValues (typeof(R2OutputLineType))) {
+				result.Add (type, new List<string>());
+			}
+			
+			foreach (string line in lines)
+			{
+				if (headerRegex.Matches (line).Count > 0) result[R2OutputLineType.R2OutputLineTypeHeader].Add (line);
+				if (rangeRegex.Matches (line).Count > 0) result[R2OutputLineType.R2OutputLineTypeRange].Add (line);
+				if (whitespaceRegex.Matches (line).Count > 0) result[R2OutputLineType.R2OutputLineTypeWhitespace].Add (line);
+			}
+			
+			return result;
 		}
 	}
 }
