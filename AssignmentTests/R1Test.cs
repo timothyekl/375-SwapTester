e7ba46ed13cb005f9db108c505fd83d4c764a36f
Tim Ekl <lithium3141@gmail.com>
2012-04-23 02:00:30 -0400

    Implement temp file wrappers for clarity
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- AssignmentTests/R1Test.cs
+++ AssignmentTests/R1Test.cs
@@ -46,36 +46,32 @@ namespace AssignmentTests
 		[Test()]
 		public void TestEmpty ()
 		{
-			string tempFile = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.empty.txt");
-			
-			this.Runner.StartApp (new string[] {tempFile});
-			
-			this.Runner.WriteInputLine ("");
-			
-			List<String> lines = this.Runner.GetOutputLines ();
-			Assert.AreEqual (1, lines.Count, "Unexpected number of lines in output");
-			
-			Regex firstLineRegex = new Regex("^Range Name: .*$");
-			Assert.IsTrue (firstLineRegex.Matches(lines[0]).Count > 0, "First line of text did not match");
-			
-			File.Delete (tempFile);
+			using(TempFileWrapper tempFile = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.empty.txt")) {
+				this.Runner.StartApp (new string[] {tempFile});
+				
+				this.Runner.WriteInputLine ("");
+				
+				List<String> lines = this.Runner.GetOutputLines ();
+				Assert.AreEqual (1, lines.Count, "Unexpected number of lines in output");
+				
+				Regex firstLineRegex = new Regex("^Range Name: .*$");
+				Assert.IsTrue (firstLineRegex.Matches(lines[0]).Count > 0, "First line of text did not match");
+			}
 		}
 		
 		[Test()]
 		public void TestFull ()
 		{
-			string tempFile = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in");
-			
-			this.Runner.StartApp (new string[] {tempFile});
-			
-			this.Runner.WriteInputLine ("");
-			
-			List<String> lines = this.Runner.GetOutputLines ();
-			Assert.AreEqual (5, lines.Count, "Unexpected number of lines in output");
-			
-			// TODO match the output more closely
-			
-			File.Delete (tempFile);
+			using(TempFileWrapper tempFile = TestHelper.ExtractResourceToTempFile ("AssignmentTests.Resources.r1-full.in")) {
+				this.Runner.StartApp (new string[] {tempFile});
+				
+				this.Runner.WriteInputLine ("");
+				
+				List<String> lines = this.Runner.GetOutputLines ();
+				Assert.AreEqual (5, lines.Count, "Unexpected number of lines in output");
+				
+				// TODO match the output more closely
+			}
 		}
 	}
 }
--- AssignmentTests/TestHelper.cs
+++ AssignmentTests/TestHelper.cs
@@ -7,7 +7,7 @@ namespace AssignmentTests
 {
 	public class TestHelper
 	{
-		public static string ExtractResourceToTempFile (string resourceName) {
+		public static TempFileWrapper ExtractResourceToTempFile (string resourceName) {
 			Assembly assembly = Assembly.GetExecutingAssembly();
 			Stream resourceStream = assembly.GetManifestResourceStream(resourceName);
 			StreamReader reader = new StreamReader(resourceStream);
@@ -18,7 +18,28 @@ namespace AssignmentTests
 				tempWriteStream.Write(text, 0, text.Length);
 			}
 			
-			return tempFileName;
+			return new TempFileWrapper(tempFileName);
+		}
+	}
+	
+	public class TempFileWrapper : IDisposable
+	{
+		public string Name {get; private set;}
+		
+		public TempFileWrapper(string name) {
+			this.Name = name;
+		}
+		
+		void IDisposable.Dispose() {
+			File.Delete(this.Name);
+		}
+		
+		public override string ToString() {
+			return this.Name;
+		}
+		
+		public static implicit operator string(TempFileWrapper t) {
+			return t.ToString ();
 		}
 	}
 }
